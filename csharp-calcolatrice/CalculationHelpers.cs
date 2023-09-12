9b9e83da793ab09b2dd934480fd91c3330fefb2c
Gaspare <Gas9220@users.noreply.github.com>
2023-09-12 15:05:12 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalculationHelpers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal sealed class CalculationHelpers
+    {
+        public static int makeIntCalculation(int firstNumber, int? secondNumber, string operation)
+        {
+
+            // If user insert two numbers
+            if (secondNumber != null)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        return firstNumber + (int)secondNumber;
+                    case "-":
+                        return firstNumber - (int)secondNumber;
+                    case "*":
+                        return firstNumber * (int)secondNumber;
+                    case "/":
+                        return firstNumber / (int)secondNumber;
+                    case "%":
+                        return firstNumber % (int)secondNumber;
+                    case "min":
+                        return Math.Min(firstNumber, (int)secondNumber);
+                    case "max":
+                        return Math.Max(firstNumber, (int)secondNumber);
+                    case "pow":
+                        return (int)Math.Pow(firstNumber, (int)secondNumber);
+                    default:
+                        throw new Exception("Something goes wrong!");
+                }
+            }
+            else
+            {
+                // if user insert one number and abs as operation
+                return Math.Abs(firstNumber);
+            }
+        }
+
+        // Helper function
+        public static int GetNumber(string numberDescription)
+        {
+            int number;
+            Console.Write($"{numberDescription} number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            return number;
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,29 @@
+﻿// Test int operations
+using csharp_calcolatrice;
+
+// Welcome
+Console.WriteLine("Enter two numbers and an operation ( values accepted +, -, *, /, %, min, max, abs)");
+
+// Get operation
+Console.Write("Operation: ");
+string operation = Console.ReadLine().ToLower();
+
+if (operation == "abs")
+{
+    int absNumber = CalculationHelpers.GetNumber("abs");
+    int result = CalculationHelpers.makeIntCalculation(absNumber, null, operation);
+    Console.WriteLine(result);
+}
+else
+{
+    // Get first number
+    int firstNumber = CalculationHelpers.GetNumber("First");
+
+    // Get second number;
+    int secondNumber = CalculationHelpers.GetNumber("Second");
+
+    // Calculation
+    int result = CalculationHelpers.makeIntCalculation(firstNumber, secondNumber, operation);
+    Console.WriteLine(result);
+}
+
